e72f2688d3af52c2d44f7f4abf525204310ffb70
jose angel <jose.albelt1817@gmail.com>
2024-05-30 14:26:06 -0500

    formulario switches: tarea xD

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
 export class SwitchesPageComponent {
+
+  public myForm: FormGroup = this.form.group({
+    gender: ['M', Validators.required],
+    wantNotfications: [true, Validators.required],
+    temsAndConditions: [false, Validators.requiredTrue],
+  });
+
+
+  constructor(
+    private form: FormBuilder,
+  ){}
+
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+
+  onSave(){
+      if(this.myForm.invalid){
+        this.myForm.markAllAsTouched();
+        return;
+      }
+
+      console.log(this.myForm.value);
+  }
 
 }
