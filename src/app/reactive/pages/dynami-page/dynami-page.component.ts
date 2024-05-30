1dcbf9efc932fba5c20910dcc4c675bffc00beae
jose angel <jose.albelt1817@gmail.com>
2024-05-30 11:10:22 -0500

    formulario dinamico: validaciones y otros metodos con arreglos

--- src/app/reactive/pages/dynami-page/dynami-page.component.ts
+++ src/app/reactive/pages/dynami-page/dynami-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { Form, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynami-page.component.html',
@@ -8,7 +8,7 @@ import { FormArray, FormBuilder, Validators } from '@angular/forms';
 export class DynamiPageComponent {
 
 
-public myForm = this.form.group({
+public myForm:FormGroup = this.form.group({
     name:['',[ Validators.required, Validators.minLength(3) ]],
     // esto ayuda si en caso dado se tiene un arreglo en el formulario
   
@@ -27,6 +27,38 @@ get favoriteGames(){
   return this.myForm.get('favoriteGame') as FormArray;
 }
 
+
+isValidField( field: string ): boolean | null {
+  return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+}
+
+
+// validar en caso formulario  con arreglo
+isValidFieldArray( formArray: FormArray, index: number){
+  return formArray.controls[index].errors
+         && formArray.controls[index].touched;
+}
+
+// VALIDA EL TIPO DE ERROR PARA ARROJAR EL MENSAJE
+getFieldError( field: string ): string | null {
+
+  if ( !this.myForm.controls[field] ) return null;
+
+  const errors = this.myForm.controls[field].errors || {};
+
+  for (const key of Object.keys(errors) ) {
+    switch( key ) {
+      case 'required':
+        return 'Este campo es requerido';
+
+      case 'minlength':
+        return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+    }
+  }
+
+  return null;
+}
 
 onSubmit(){
 
