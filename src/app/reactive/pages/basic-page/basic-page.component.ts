d791fc9c06e621a3e3c0d852e70e1606ee15168b
jose angel <jose.albelt1817@gmail.com>
2024-05-30 09:40:12 -0500

    formilario basico: forma de hacerlos y validadores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent  {
+
+  //TODO: forma 1 
+  // public myForm: FormGroup = new FormGroup({
+  //     name: new FormControl(''),
+  //     price: new FormControl(0,),
+  //     inStoraje: new FormControl(0),
+  // })
+
+
+  //TODO: forma 2
+  public myForm:FormGroup = this.fb.group({
+    name:['',[Validators.required, Validators.minLength(3)]] ,
+    price: [0, [Validators.required, Validators.minLength(0)]],
+    inStoraje: [0,[Validators.required, Validators.minLength(0)]],
+  })
+
+
+  constructor (private fb: FormBuilder){}
+
+ 
+  
+
+
+  onSave():void{
+    if(this.myForm.invalid) return;
+    console.log(this.myForm.value)
+
+    
+  }
+
+
+
 
 }
