2314a750f16eed5ceb56b3e46640214b9dea17f2
jose angel <jose.albelt1817@gmail.com>
2024-05-30 16:07:11 -0500

    validadores asincronos  - validar el email

--- src/app/shared/validators/email-validaitor.service.ts
+++ src/app/shared/validators/email-validaitor.service.ts
@@ -1,31 +1,62 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { Observable, delay, of } from 'rxjs';
+import { delay, Observable, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class EmailValidatorService implements AsyncValidator {
     
     
-    validate(control: AbstractControl):  Observable<ValidationErrors | null> {
-        
+    validate(control: AbstractControl ): Observable<ValidationErrors | null> {
+
         const email = control.value;
-        console.log('email',email);
-        return of(
-            {
-                emailTake:true
-            }
-        ).pipe(
-            delay( 2000 )
-        )
-
+    
+        const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) => {
+    
+          console.log({ email });
+    
+          if ( email === 'joseangel@google.com' ) {
+            subscriber.next({ emailTaken: true });
+            subscriber.complete();
+            // return;
+          }
+    
+          subscriber.next(null);
+          subscriber.complete();
+    
+    
+        }).pipe(
+          delay( 1000 )
+        );
+    
+        return httpCallObservable;
+    
+      }
+    
+    
+    
+      // validate(control: AbstractControl ): Observable<ValidationErrors | null> {
+    
+      //   const email = control.value;
+      //   console.log({ email })
+    
+      //   return of({
+      //     emailTaken: true
+      //   }).pipe(
+      //     delay( 2000 )
+      //   );
+    
+      // }
+    
+    
     }
-
-    // // esto es opcional 
-    // registerOnValidatorChange?(fn: () => void): void {
-    //     throw new Error('Method not implemented.');
-    // }
-
     
     
-
-}
+    // return this.http.get<any[]>(`http://localhost:3000/users?q=${ email }`)
+    // .pipe(
+    //   // delay(3000),
+    //   map( resp => {
+    //     return ( resp.length === 0 )
+    //         ? null
+    //         : { emailTaken: true }
+    //   })
+    // );
